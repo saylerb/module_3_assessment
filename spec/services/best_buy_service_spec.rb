31581e1daea315fe550775c22fe1607e98b95ab7
Brian Sayler <sayler.b@gmail.com>
2016-08-02 10:58:40 -0600

    beef up service spec

--- spec/services/best_buy_service_spec.rb
+++ spec/services/best_buy_service_spec.rb
@@ -5,9 +5,19 @@ RSpec.describe BestBuyService do
   it "returns a list of stores" do
     VCR.use_cassette("best_buy_stores") do
       all_store_data = BestBuyService.new.get_store_data("80202")
-      store = all_store_data[:stores].first
+
+      stores = all_store_data[:stores]
+
+      expect(stores.length).to eq(15)
+
+      store = stores.first
 
       expect(store[:storeId]).to eq(2740)
+      expect(store[:longName]).to eq("Best Buy Mobile - Cherry Creek Shopping Center")
+      expect(store[:city]).to eq("Denver")
+      expect(store[:distance]).to eq(3.25)
+      expect(store[:phone]).to eq("303-270-9189")
+      expect(store[:storeType]).to eq("Mobile")
     end
   end
 end
