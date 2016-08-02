72871c2091cbdeb50a84cf12f8bd8737b58cc4df
Brian Sayler <sayler.b@gmail.com>
2016-08-02 11:53:10 -0600

    add store search class and search results view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,11 @@
 class SearchController < ApplicationController
   def index
-    @stores = Store.all
+    @stores, @total_stores = StoreSearch.all(search_params[:query])
+  end
+
+  private
+
+  def search_params
+    params.require(:search).permit(:query)
   end
 end
