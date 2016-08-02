cdb62dc4ce108a93bb253dfa9f35c0cc0dd771ed
Brian Sayler <sayler.b@gmail.com>
2016-08-02 10:52:53 -0600

    fix service spec to take in zip code, and expect actual store id taken from postman

--- spec/services/best_buy_service_spec.rb
+++ spec/services/best_buy_service_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe BestBuyService do
 
   it "returns a list of stores" do
     VCR.use_cassette("best_buy_stores") do
-      all_store_data = BestBuyService.new.get_store_data
+      all_store_data = BestBuyService.new.get_store_data("80202")
       store = all_store_data[:stores].first
 
-      expect(store[:storeId]).to eq(100)
+      expect(store[:storeId]).to eq(2740)
     end
   end
 end
